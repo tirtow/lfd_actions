0b886a01f5e225bd9106f6b48f39a6ad3426a4cb
Michael Tirtowidjojo <mtirtowidjojo@gmail.com>
2016-11-28 16:52:30 -0600

    Use all joints

--- src/dtw.cpp
+++ src/dtw.cpp
@@ -111,12 +111,15 @@ double DTW::quaternion_distance(const Quaternion& c, const Quaternion& d) {
 }
 
 double DTW::joint_distance(const JointState& x, const JointState& y) {
-    double pos_diff = abs((x.position[6] - y.position[6])
-            + (x.position[7] - y.position[7]));
-    double vel_diff = abs((x.velocity[6] - y.velocity[6])
-            + (x.velocity[7] - y.velocity[7]));
-    double eff_diff = abs((x.effort[6] - y.effort[6])
-            + (x.effort[7] - y.effort[7]));
+    double pos_diff = 0;
+    double vel_diff = 0;
+    double eff_diff = 0;
+
+    for (int i = 0; i < 8; i++) {
+        pos_diff += abs(x.position[i] - y.position[i]);
+        vel_diff += abs(x.velocity[i] - y.velocity[i]);
+        eff_diff += abs(x.effort[i] - y.effort[i]);
+    }
 
     return pos_diff + vel_diff + eff_diff;
 }
